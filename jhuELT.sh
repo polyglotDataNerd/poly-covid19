359163263cff37f386371a87d36076fcc7da04d0
polyglotDataNerd <gerardnino@gmail.com>
2020-04-03 07:02:04 -0700

    daily JHU repo pull to  data up to s3

---
+++ jhuELT.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# local RUN
+CURRENTDATE="$(date -v -1d +%m-%d-%Y)"
+cd ~/external/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/
+git pull
+aws s3 cp ~/external/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/"$CURRENTDATE".csv s3://poly-testing/covid/jhu/raw/"$CURRENTDATE".csv --sse
+
+cd ~/
