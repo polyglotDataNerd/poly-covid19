a6ea6dff8ac46cdba91cb9504bd5af12b640b766
polyglotDataNerd <gerardnino@gmail.com>
2020-04-06 07:18:26 -0700

    changed bash ETL to run all

--- ELT.sh
+++ ELT.sh
@@ -5,4 +5,6 @@ cd ~/external/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/
 git pull
 aws s3 cp ~/external/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/"$CURRENTDATE".csv s3://poly-testing/covid/jhu/raw/"$CURRENTDATE".csv --sse
 
+# runs go package
+go run ~/solutionsgo/poly-covid19/Entry.go
 cd ~/
